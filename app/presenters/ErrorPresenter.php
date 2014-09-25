c0a0010b223afb7f92fbf136dae8c57b54777afc
Martin Štekl <martin.stekl@gmail.com>
2014-09-25 22:53:24 +0200

    Fixed usage of logger in ErrorPresenter

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -3,7 +3,7 @@
 namespace App\Presenters;
 
 use Nette;
-use Tracy\ILogger;
+use Nette\Diagnostics\Debugger;
 
 
 /**
@@ -11,19 +11,6 @@ use Tracy\ILogger;
  */
 class ErrorPresenter extends BasePresenter
 {
-
-	/**
-	 * @var ILogger
-	 */
-	private $logger;
-
-
-
-	public function __construct(ILogger $logger)
-	{
-		$this->logger = $logger;
-	}
-
 
 	/**
 	 * @param  Exception
@@ -36,11 +23,11 @@ class ErrorPresenter extends BasePresenter
 			// load template 403.latte or 404.latte or ... 4xx.latte
 			$this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx');
 			// log to access.log
-			$this->logger->log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
+			Debugger::log("HTTP code $code: {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}", 'access');
 
 		} else {
 			$this->setView('500'); // load template 500.latte
-			$this->logger->log($exception, ILogger::EXCEPTION); // and log exception
+			Debugger::log($exception, Debugger::ERROR); // and log exception
 		}
 
 		if ($this->isAjax()) { // AJAX request? Note this error in payload.
