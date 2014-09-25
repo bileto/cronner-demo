ea34b66f8ab01ecf01eadbe64596f4fc32eee61e
Martin Štekl <martin.stekl@gmail.com>
2014-09-25 20:48:41 +0200

    Implemented usage of Cronner in kdyby\console

---
+++ app/commands/CronnerCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Commands;
+
+use CronnerTasks\DummyTasks;
+use stekycz\Cronner\Cronner;
+use stekycz\Cronner\Tasks\Task;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+
+class CronnerCommand extends Command
+{
+
+	/**
+	 * @var \stekycz\Cronner\Cronner
+	 * @inject
+	 */
+	public $cronner;
+
+
+
+	protected function configure()
+	{
+		$this->setName('cronner:run')
+			->setDescription('Runs all Cronner tasks');
+	}
+
+
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$this->cronner->onTaskFinished[] = function (Cronner $cronner, Task $task) use ($output) {
+			$output->writeln('Task ' . $task->getName() . ' has been finished.');
+		};
+		$this->cronner->onTaskError[] = function (Cronner $cronner, \Exception $exception, Task $task) use ($output) {
+			$output->writeln('<error>Task "' . $task->getName() . '" has been stoped by an error: ' . $exception->getMessage() . '</error>');
+		};
+		$this->cronner->addTasks(new DummyTasks());
+		$this->cronner->run();
+		$output->writeln('<info>Cronner finished</info>');
+
+		return 0;
+	}
+
+}
